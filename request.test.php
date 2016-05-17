fe3f34c25d48389121b7237a5cf74660a80e2456
labodudev <lelabodudev@gmail.com>
2016-05-17 16:38:40 +0200

    Add comment /* and //

--- request.test.php
+++ request.test.php
@@ -29,13 +29,16 @@ foreach ( $unitList as $file_url )
 				!preg_match( '#\\/\/#', $lines[$key] ) &&
 				!preg_match( '#\( ?int ?\)#', $lines[$key] ) &&
 				!preg_match( '#\( ?array ?\)#', $lines[$key] ) &&
-				!preg_match( '#intval#', $lines[$key] ) ) {
+				!preg_match( '#intval#', $lines[$key] ) &&
+				!preg_match( '#varSanitizer#', $lines[$key] ) ) {
 				  $string_post_unsecured[$file_url][$key + 1] = htmlentities( $lines[$key] );
 				  $total_unsecured_line++;
 			  }
 
 			  if ( preg_match( '#(\$_POST|\$_GET|\$_REQUEST)\[\'.+\'\].+?\=#isU', $lines[$key] ) &&
-        !preg_match( '#\* @#', $lines[$key] ) ) {
+        !preg_match( '#\* @#', $lines[$key] ) &&
+        !preg_match( '#\\/\/#', $lines[$key] ) &&
+        !preg_match( '#\*#', $lines[$key] ) ) {
   				$string_post_unsecured[$file_url][$key + 1] = htmlentities( $lines[$key] );
   				$total_unsecured_line++;
 			  }
