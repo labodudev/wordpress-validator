c96c6afd0d5eb005a1b24f9e702100dc3fa76b77
labodudev <lelabodudev@gmail.com>
2016-05-19 10:26:39 +0200

    Update request and nonce test

--- nonce.test.php
+++ nonce.test.php
@@ -18,21 +18,22 @@ if ( $test != '../test/request.test.php' ) {
 	$string_post_unsecured[$test] = array();
 
     $file = file_get_contents( $test );
-    preg_match_all( '#add_action\(.+(\'|")wp_ajax_.+(\'|"),.+(\'|")(.+)(\'|").+\)#isU', $file, $matches );
+    preg_match_all( '#add_action\( *(\'|")wp_ajax_(nopriv_)?.+(\'|"),.+(\'|")(.+)(\'|").+\)#isU', $file, $matches );
 	$matched_function = array();
-	if ( !empty( $matches[4] ) ) {
-		foreach ( $matches[4] as $matched_string ) {
+	if ( !empty( $matches[5] ) ) {
+		foreach ( $matches[5] as $matched_string ) {
 			preg_match_all( '#(.+)?\', *(.+)?\'#isU', $matched_string, $exploded );
 			if ( empty( $exploded[0] ) ) {
 				$matched_string = str_replace( '(', '', $matched_string );
 				$matched_string = str_replace( ')', '', $matched_string );
 				$matched_string = trim($matched_string);
-				preg_match( '#function *' . $matched_string . '(.+)?\}#isU', $file, $function );
+				preg_match( '#function *' . $matched_string . ' *\((.+)?\}#isU', $file, $function );
 
 				if ( !empty( $function[0] ) ) {
 					$function = $function[0];
-					if ( !preg_match( '#wp_verify_nonce#', $function ) ) {
-						$string_post_unsecured[$test][$matched_string] = "wp_security_nonce() not found";
+					if ( !preg_match( '#wp_verify_nonce#', $function ) &&
+					 	!preg_match( '#check_ajax_referer#', $function ) ) {
+						$string_post_unsecured[$test][$matched_string] = "wp_security_nonce() or check_ajax_referer not found";
 						$total_unsecured_line++;
 					}
 				}
@@ -50,7 +51,7 @@ if ( !empty( $string_post_unsecured ) ) {
     if ( !empty( $file ) ) {
       echo "[+] File : " . $name_file . ' => Unsecured nonce ' . count( $file ) . PHP_EOL . '<br />';
       foreach ( $file as $fonction_name => $content ) {
-        echo "[+] Not found wp_verify_nonce on : " . $fonction_name . PHP_EOL . '<br />';
+        echo "[+] <span style='color: #ea6153'>Not found wp_verify_nonce on : " . $fonction_name . PHP_EOL . '</span><br />';
       }
     }
   }
--- request.test.php
+++ request.test.php
@@ -25,10 +25,12 @@ foreach ( $unitList as $file_url )
 	      $lines[$key] = preg_replace( '#!empty\(.+?(\$_POST|\$_GET|\$_REQUEST)\[\'.+\'\].+?\) \?#isU', '', $lines[$key] );
 			if ( $file_url != "..\\test\\request.test.php" ) {
 			  if ( !preg_match( '#sanitize_.+#', $lines[$key] ) &&
+        !preg_match( '#esc_.+#', $lines[$key] ) &&
 				!preg_match( '#\*#', $lines[$key] ) &&
 				!preg_match( '#\\/\/#', $lines[$key] ) &&
 				!preg_match( '#\( ?int ?\)#', $lines[$key] ) &&
 				!preg_match( '#\( ?array ?\)#', $lines[$key] ) &&
+				!preg_match( '#\( ?float ?\)#', $lines[$key] ) &&
 				!preg_match( '#intval#', $lines[$key] ) &&
 				!preg_match( '#varSanitizer#', $lines[$key] ) ) {
 				  $string_post_unsecured[$file_url][$key + 1] = htmlentities( $lines[$key] );
@@ -55,7 +57,21 @@ if ( !empty( $string_post_unsecured ) ) {
     if ( !empty( $file ) ) {
       echo "[+] File : " . $file_url . ' => Unsecured $_POST|$_GET|$_REQUEST ' . count( $file ) . PHP_EOL . '<br />';
       foreach ( $file as $line => $content ) {
-        echo "[+] Line : " . $line . " => " . trim($content) . PHP_EOL . '<br />';
+        $color = "black";
+        if ( preg_match( '#\$_POST#', trim($content) ) ) {
+          $color = "#ea6153";
+        }
+        else if( preg_match( '#\$_GET#', trim($content) ) ) {
+          $color = "#3498db";
+        }
+        else if( preg_match( '#\$_REQUEST#', trim($content) ) ) {
+          $color = "#2ecc71";
+        }
+        else if( preg_match( '#\$_SESSION#', trim($content) ) ) {
+          $color = "#f1c40f";
+        }
+
+        echo "[+] <span style='color: " . $color . "'>Line : " . $line . " => " . trim($content) . PHP_EOL . '</span><br />';
       }
     }
   }
